501e3d16db86f12ef4879844100ae55d77543579
KrypticCliff <Clifton.raw@gmail.com>
2018-11-22 19:30:32 -0800

    Iterates through backup files to create empty folder

--- ext_sorter.cpp
+++ ext_sorter.cpp
@@ -12,15 +12,45 @@ int main(int argc, char** argv) {
 
     std::cout << "Location: " << argv[1] << "\n";
     fs::path p(argv[1]);
-    fs::directory_iterator begin(p), end;
+    fs::directory_iterator root_begin(p), end;
 
 //  Checks if directory path is a directory
-    if (!is_directory(p)) {
+    if (!fs::is_directory(p)) {
         std::cerr << "Could not find location.\n";
         return -1;
     }
 
-    for (; begin != end; begin++) {
-        std::cout << begin->path().extension() << "\n";
+    try {
+        // Iterates through ROOT directory
+        for (; root_begin != end; root_begin++) {
+            std::cout << root_begin->path().string() << "\n";
+            fs::directory_iterator sub_begin(root_begin->path());
+
+            // Iterates through SUB directory (Assuming filename recup_dir.#)
+            for (; sub_begin != end; sub_begin++) {
+                // Remove "." from file extension
+                // If folder !exists, create folder called file_ext
+
+                std::string file_ext = sub_begin->path().extension().string();
+                file_ext.erase(0,1);
+                fs::path ext_p(file_ext);
+                //std::cout << sub_begin->path().extension().string() << "\n";
+
+                // Check if extension directory exists
+                if (!fs::exists(ext_p)) {
+                    fs::create_directory(ext_p);
+                }
+                else {
+                    continue;
+                }
+
+
+            } // For Loop 1
+
+        } // For Loop 2
+
+    } // Try
+    catch (int e) {
+        std::cout << "An exception has occurred: " << e << "\n";
     }
-}
+} // Main
