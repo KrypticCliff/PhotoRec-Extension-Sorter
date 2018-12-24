8a1cf5fbd783c138714989de6e74c36c1091bdee
KrypticCliff <Clifton.raw@gmail.com>
2018-12-23 22:56:14 -0800

    File Extension Sort Temp Fix

--- ext_sorter.cpp
+++ ext_sorter.cpp
@@ -3,6 +3,35 @@
 
 namespace fs = boost::filesystem;
 
+fs::path file_exists(fs::path p, std::string ext, std::string file_name) {
+    fs::path file_dest(p/ext/file_name);
+
+    if (fs::exists(file_dest)) {
+        int counter = 1;
+
+        while (fs::exists(file_dest)) {
+            std::string file_name = file_dest.stem().string();
+            std::string count_ext = "_" + std::to_string(counter) + "." + ext;
+
+            if (counter < 2) {
+                file_name = file_name + count_ext;
+            } else {
+                std::cout << "Before: " << file_name.length() << "\n";
+                file_name.erase(count_ext.length(), count_ext.length());
+                std::cout << "After: " << file_name.length() << "\n";
+                file_name = file_name + count_ext;
+                // std::cout << file_name.size() - count_ext.size() << "\n";
+            }
+
+            counter++;
+            file_dest = (p/ext/file_name);
+        }
+        return file_dest;
+    } else
+        return file_dest;
+
+} // If Statement
+
 int main(int argc, char** argv) {
 //  Checks if 1 path to location is found.
     if (argc < 2 || argc >= 3) {
@@ -31,34 +60,25 @@ int main(int argc, char** argv) {
             std::string file_name = sub_begin->path().filename().string();
             std::string file_ext  = sub_begin->path().extension().string();
 
-            // Remove "." from file extension
-            file_ext.erase(0,1);
+            // Remove "." from file extension named
+            std::string _file_ext = file_ext.erase(0,1);
 
             // Test statement
-            std::cout << sub_begin->path() << "\n";
+            //std::cout << sub_begin->path() << "\n";
 
             // If folder !exists, create folder extension folder
-            if (!fs::exists(p/file_ext)) {
-                fs::create_directory(p/file_ext);
+            if (!fs::exists(p/_file_ext)) {
+                fs::create_directory(p/_file_ext);
             }
 
             // file_d "backup_path/name_of_ext/filename"
-            // file_s iterated file 
-            fs::path file_dest(p/file_ext/file_name);
+            // fs::path file_dest(p/file_ext/file_name);
+            fs::path file_dest(file_exists(p, file_ext, file_name));
             fs::path file_src(sub_begin->path());
 
             fs::copy(file_src, file_dest);
 
-/*            fs::directory_iterator inner_sub_p("./");
-            for (; inner_sub_p != end; inner_sub_p++) {
-                std::string sub_file_name = inner_sub_p->path().extension().string();
-                if (file_ext == sub_file_name) {
-
-                }
-            }
-*/
-
-            //fs::rename(sub_begin->path(),)
+// Current issue: Same name files cause issues.
         } // For Loop 1
     } // For Loop 2
 } // Main
