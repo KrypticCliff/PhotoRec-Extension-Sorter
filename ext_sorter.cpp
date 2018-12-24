87a7fc74b2541a79a4e473a74d84251d5170afbf
KrypticCliff <Clifton.raw@gmail.com>
2018-12-23 23:19:55 -0800

    Clean Up File

--- ext_sorter.cpp
+++ ext_sorter.cpp
@@ -1,8 +1,16 @@
+/*
+    Current issues: Duplicate Files may add additional "_#" tags on name.
+    Files within sub_begin will not be accounted for and
+    may cause issues.
+*/
+
 #include <iostream>
 #include <boost/filesystem.hpp>
 
 namespace fs = boost::filesystem;
 
+
+// Function checks if file exists, if so iterate name i.e. file_1, file_2.
 fs::path file_exists(fs::path p, std::string ext, std::string file_name) {
     fs::path file_dest(p/ext/file_name);
 
@@ -15,25 +23,25 @@ fs::path file_exists(fs::path p, std::string ext, std::string file_name) {
 
             if (counter < 2) {
                 file_name = file_name + count_ext;
+
             } else {
-                std::cout << "Before: " << file_name.length() << "\n";
                 file_name.erase(count_ext.length(), count_ext.length());
-                std::cout << "After: " << file_name.length() << "\n";
                 file_name = file_name + count_ext;
-                // std::cout << file_name.size() - count_ext.size() << "\n";
             }
 
             counter++;
             file_dest = (p/ext/file_name);
-        }
-        return file_dest;
-    } else
-        return file_dest;
+        } // While Statement
 
+        return file_dest;
+    }
+
+    return file_dest;
 } // If Statement
 
+
 int main(int argc, char** argv) {
-//  Checks if 1 path to location is found.
+// Checks if 1 path to location is found.
     if (argc < 2 || argc >= 3) {
         std::cout << "Input Location of Path to Sort.\n";
         return -1;
@@ -44,41 +52,38 @@ int main(int argc, char** argv) {
     fs::path cur_p("./");
     fs::directory_iterator root_begin(p), end;
 
-//  Checks if directory path is a directory
+// Checks if directory path is a directory
     if (!fs::is_directory(p)) {
         std::cerr << "Could not find location.\n";
         return -1;
     }
 
-    // Iterates through ROOT directory
+// Iterates through ROOT directory
     for (; root_begin != end; root_begin++) {
         //std::cout << root_begin->path().string() << "\n";
         fs::directory_iterator sub_begin(root_begin->path());
 
-        // Iterates through SUB directory (Assuming filename recup_dir.#)
+// Iterates through SUB directory (Assuming filename recup_dir.#)
         for (; sub_begin != end; sub_begin++) {
             std::string file_name = sub_begin->path().filename().string();
             std::string file_ext  = sub_begin->path().extension().string();
 
-            // Remove "." from file extension named
+// Remove "." from file extension named
             std::string _file_ext = file_ext.erase(0,1);
 
-            // Test statement
-            //std::cout << sub_begin->path() << "\n";
+// Test statement
+            std::cout << sub_begin->path() << "\n";
 
-            // If folder !exists, create folder extension folder
-            if (!fs::exists(p/_file_ext)) {
+// If folder !exists, create folder extension folder
+            if (!fs::exists(p/_file_ext))
                 fs::create_directory(p/_file_ext);
-            }
 
-            // file_d "backup_path/name_of_ext/filename"
-            // fs::path file_dest(p/file_ext/file_name);
+// file_dest = "backup_path/name_of_ext/filename"
             fs::path file_dest(file_exists(p, file_ext, file_name));
             fs::path file_src(sub_begin->path());
 
             fs::copy(file_src, file_dest);
 
-// Current issue: Same name files cause issues.
         } // For Loop 1
     } // For Loop 2
 } // Main
